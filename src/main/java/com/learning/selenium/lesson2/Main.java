16aa9ff3c427ef4a51a3a6a218077ab2c7800981
Шевцов Анатолий Александрович <ashev95@yandex.ru>
2021-12-09 22:39:41 +0500

    Авторизация в веб-приложении litecart

--- src/main/java/com/learning/selenium/lesson2/Main.java
+++ src/main/java/com/learning/selenium/lesson2/Main.java
@@ -1,9 +1,37 @@
 package com.learning.selenium.lesson2;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
 public class Main {
 
+    private static final String URL = "http://localhost:8080/litecart/admin";
+    private static final String LOGIN = "admin";
+    private static final String PASSWORD = "admin";
+
     public static void main(String args[]) {
-        System.out.println("Hello, World!");
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+        WebDriver driver = null;
+        try {
+            driver = new ChromeDriver();
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(3));
+            driver.navigate().to(URL);
+            driver.findElement(By.name("username")).sendKeys(LOGIN);
+            driver.findElement(By.name("password")).sendKeys(PASSWORD);
+            driver.findElement(By.name("remember_me")).click();
+            driver.findElement(By.name("login")).click();
+            wait.until(webDriver -> ((JavascriptExecutor) webDriver).executeScript("return document.readyState").equals("complete"));
+        } finally {
+            if (driver != null) {
+                driver.quit();
+                driver = null;
+            }
+        }
     }
 
 }
